df703cd36b39a8f6adb178165931dda42645508a
Ali Almarzooq <94115088+Ali100i@users.noreply.github.com>
2022-10-20 01:36:21 +0300

    resolving errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
